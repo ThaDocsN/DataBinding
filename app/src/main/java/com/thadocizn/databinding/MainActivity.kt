6e80be25876828a567c217ace29fe163eb4ae30a
ThaDocIzN <rodan72@msn.com>
2019-05-15 09:38:12 -0500

    implemented binding on buttons

--- app/src/main/java/com/thadocizn/databinding/MainActivity.kt
+++ app/src/main/java/com/thadocizn/databinding/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.thadocizn.databinding
 
+import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.thadocizn.databinding.databinding.ActivityMainBinding
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding:ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.user = UserData("Charles Johnson", "Wrote this")
 
+        val handlers = MainActivityClickHandlers(this)
+        binding.clickHandler = handlers
+
+    }
+
+    class MainActivityClickHandlers(val context: Context){
+        fun onEnroll(view: View){context.toast("This is a test")}
+        fun onCancel(view: View) {context.toast("This is test 2")}
     }
 }
